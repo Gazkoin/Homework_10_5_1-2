d419f1ef0bdf9f49c7b53368c028031c91061cec
Gazkoin <pshu_timur@mail.ru>
2026-04-18 22:33:46 +0300

    Добавьте файлы проекта.

---
+++ Calculate.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Homework_10_5_1_2
+{
+    
+    public interface CalcFunc
+    {
+        void Plus (double x, double y);
+        void Log (bool k);
+    }
+
+    internal class Calculate
+    {
+        public void Plus (double x, double y)
+        {
+            Console.WriteLine($"{x}+{y}={x+y}");
+        }
+        public void Log(bool k)
+        {
+            if (k) Console.ForegroundColor = ConsoleColor.Blue;
+            else Console.ForegroundColor = ConsoleColor.Red;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace Homework_10_5_1_2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Calculate calculate = new Calculate();
+            while (true)
+            {
+                try
+                {
+                    calculate.Log(true);
+                    Console.WriteLine("\nВведите х (0 для выхода):");
+                    double x = double.Parse(Console.ReadLine().Trim());
+                    if (x == 0) break;
+
+                    Console.WriteLine("Введите y (0 для выхода):");
+                    double y = double.Parse(Console.ReadLine().Trim());
+
+                    calculate.Plus(x, y);
+                }
+                catch (Exception ex) { calculate.Log(false); Console.WriteLine($"[LOG] {DateTime.Now}: {ex.Message}"); } 
+            }                      
+            
+        }
+    }
+}
